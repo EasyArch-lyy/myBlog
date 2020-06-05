bb2254cb1cce2fabbbe78d4935aacbdc4d3d9970
sx-9650 <sx-liuyy@dtdream.com>
2020-06-05 18:32:33 +0800

    first commit

---
+++ src/main/java/com/jinxiu/blog/base/UserController.java
@@ -0,0 +1,15 @@
+package com.jinxiu.blog.base;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**
+ * 博客登录校验
+ */
+@RestController
+@RequestMapping('/login')
+public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+    private static final String RESULT = "result";
+    private static final String MESSAGE = "message";
+
+
+}
